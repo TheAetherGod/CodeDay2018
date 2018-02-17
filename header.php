948988872c9cecce93b1cbd40e0465b753b3a6e8
Benwagrez <benwagrez@gmail.com>
2018-02-17 17:37:07 -0600

    heres broken shit

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <div id = "banner" style = "width:100%">
 	<img id = "LPLogo" src = "https://www.lphs.org/cms/lib/IL01904769/Centricity/Template/GlobalAssets/images///logos/_default.png">
         
-<h1 class = "baseTitle" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:64px; text-shadow: 2px 2px 4px #000;"><span id = "LPNHS" style = "cursor: pointer;" onclick = "location.href='index.php'">Make It Happen</span></h1>
-<?php if(isset($_SESSION["Username"])) : ?>
-	<div id = "headerLogout" class = "headerSignIn"><button id = "headerLogoutButton">User Profile</button></div>
+<h1 class = "baseTitle" style = "padding-bottom: 0px; margin-bottom:0px; color: #17252A;  font-size:64px; text-shadow: 2px 2px 4px #000;"><span id = "LPNHS" style = "cursor: pointer;" onclick = "location.href='index.php'">Make It Happen</span></h1>
+<?php if(isset($_SESSION["userID"])) : ?>
+	<div id = "headerLogout" class = "headerSignIn"><a class = "headerLogoutButton">User Profile</a></div>
 	<?php else: ?>
-		<div id = "headerLogin" class = "headerSignIn"><button id = "headerLoginButton">Sign In</button></div>
+		<div id = "headerLogin" class = "headerSignIn"><a class = "headerLoginButton" href = "login.php" style="text-decoration:none;padding:5px;">Sign In</a></div>
 	<?php endif; ?>
 
 </div>
