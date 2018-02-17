5a68eaf4dda27a188e6470815474cf6ad6b8cea8
Kush <shahkush56@gmail.com>
2018-02-17 16:57:40 -0600

    fixed the footer

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 <h4 class = "baseText" style = "font-size: 24px">Make It Happen</h4>
     <p class = "footerText">Created by Ben, Kush, Luis, Salvador</p>
-=======
-<h4 class = "baseText" style = "font-size: 24px">Lake Park National Honor Society Webpage</h4>
-    <p class = "footerText">Created by Sahil Patel and Ben Wagrez</p>
->>>>>>> parent of 059a3d9... Text test
+
     
     <!--Notice that this page is UNOFFICIAL-->
     <p style = "color: white;">***This website was created at Codeday Chicago 2018***</p>
--- homePage.php
+++ homePage.php
@@ -25,21 +25,21 @@
 
 <body>
     <!--Fixed Img in Background-->
-    <img id = "fixedBGImg" src = "https://www.nhs.us/assets/images/nhs/NHS_header_logo.png">
     
 	<div id = "footerPusher">
 		<!--Home Page Main Img Card-->
 		<div id = "frontImg" class = "card" style = "width: 79%;">
-			<p style = "font-style: italic; font-size: 16px;">Promoting appropriate recognition of students who reflect outstanding accomplishments in the areas of scholarship, leadership, character, and service.</p>
+			<p style = "font-style: italic; font-size: 16px; text-align: left">Feed: Looking for popular events? </p>
 		</div>
 
 		<!--Home Page Panels-->
 		<div id = "importantInfo" class = "urgent panel">
-			<p class = "urgentText">Attention:</p>
+			<p style = "text-align: left" class = "urgentText">Web discription:</p>
 			<p class = "urgentText"></p>
 		</div>
 		<div id = "aboutUs" class = "classic panel">
-			<p>About Us...</p>
+            <p>About Us...</p>
+            <p>hello</p>
 		</div>
 	</div>
 </body>
