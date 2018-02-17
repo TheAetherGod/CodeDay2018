059a3d9d3b58746fcb0bf93eb8fd287c70d159e2
TheAetherGod <33580943+TheAetherGod@users.noreply.github.com>
2018-02-17 14:25:15 -0600

    Text test
    Teaching users how to change text

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<h4 class = "baseText" style = "font-size: 24px">Lake Park National Honor Society Webpage</h4>
+<h4 class = "baseText" style = "font-size: 24px">Suck Butts</h4>
     <p class = "footerText">Created by Sahil Patel and Ben Wagrez</p>
     
     <!--Notice that this page is UNOFFICIAL-->
