a7f2c319f858b8be638a9663e2047f75db51e503
Benwagrez <benwagrez@gmail.com>
2018-02-17 14:01:22 -0600

    added base code and new file structure

---
+++ header.php
@@ -0,0 +1,28 @@
+<div id = "banner" style = "width:100%">
+	<img id = "LPLogo" src = "https://www.lphs.org/cms/lib/IL01904769/Centricity/Template/GlobalAssets/images///logos/_default.png">
+        
+	<h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:48px;"><span id = "LPNHS" onclick = "location.href='index.php'" title = "LPNHS - Home">Lake Park High School National Honor Society</span></h1>
+	<h2 class = "baseText" style = "padding-top: 0px; margin-top:0px; color: #666; font-size:18px;">Scholarship | Leadership | Character | Service</h2>
+	<?php if(isset($_SESSION["StudentID"])) : ?>
+		<div id = "headerLogout" class = "headerSignIn"><button id = "headerLogoutButton">Sign Out</button></div>
+	<?php else: ?>
+		<div id = "headerLogin" class = "headerSignIn"><button id = "headerLoginButton">Sign In</button></div>
+	<?php endif; ?>
+
+</div>
+    
+<div id = "navBarWrapper">
+    <nav id = "navBar" class = "topnav">
+        <?php if(isset($_SESSION["StudentID"])) : ?>
+            <a class = "baseText" id = "homeLink" href = "index.php">Home</a>
+            <a class = "baseText" id = "eventsLink" href = "events.php">Community Events</a>
+            <a class = "baseText" id = "membersLink" href = "members.php">Members</a>
+			<a class = "baseText" id = "myProfileLink" href = "my-profile.php">My Profile</a>
+        <?php else :?>
+            <a class = "baseText" id = "homeLink" href = "index.php">Home</a>
+            <a class = "baseText" id = "communityInvolvementLink" href = "community-involvement.php">Community Events</a>
+            <a class = "baseText" id = "membersLink" href = "members.php">Members</a>
+			<a class = "baseText" id = "whatItTakesLink" href = "what-it-takes.php">What It Takes</a>
+        <?php endif; ?>
+    </nav>
+</div>
---
+++ homePage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>LPNHS - Home</title>
+    
+    <!--TODO: Icon-->
+    
+    
+    <!--Style Sheets-->
+    <link rel="stylesheet" href="homePage.css">
+    
+    <!--Scripts-->
+    <!--jQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="headerJQuery.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#homeLink").addClass("active");
+        });
+    </script>
+</head>
+
+<!--Included via PHP-->
+<header id = "header"><?php include "header.php"; ?></header>
+
+<body>
+    <!--Fixed Img in Background-->
+    <img id = "fixedBGImg" src = "https://www.nhs.us/assets/images/nhs/NHS_header_logo.png">
+    
+	<div id = "footerPusher">
+		<!--Home Page Main Img Card-->
+		<div id = "frontImg" class = "card" style = "width: 50%;">
+		   <img src = "https://www.lphs.org/cms/lib/IL01904769/Centricity/Domain/70/NHS%202017.jpg" style = "width: 100%;">
+			<p style = "font-style: italic; font-size: 16px;">Promoting appropriate recognition of students who reflect outstanding accomplishments in the areas of scholarship, leadership, character, and service.</p>
+		</div>
+    
+		<!--Home Page Panels-->
+		<div id = "importantInfo" class = "urgent panel">
+			<p class = "urgentText">Attention:</p>
+			<p class = "urgentText"></p>
+		</div>
+        <?php else: endif; ?>
+		<div id = "aboutUs" class = "classic panel">
+			<p>About Us...</p>
+			<p><?php echo $aboutus; ?></p>
+		</div>
+	</div>
+</body>
+
+<!--Included via PHP-->
+<footer id = "footer"><?php include "footer.php"; ?></footer>
+</html>
