7d03f0d0528ef8e2201990111366f16308d16cf7
Benwagrez <benwagrez@gmail.com>
2018-02-17 16:06:39 -0600

    edits to header and added new file

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <div id = "banner" style = "width:100%">
 	<img id = "LPLogo" src = "https://www.lphs.org/cms/lib/IL01904769/Centricity/Template/GlobalAssets/images///logos/_default.png">
         
-	<h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:48px;"><span id = "LPNHS" onclick = "location.href='index.php'" title = "LPNHS - Home">Make It Happen</span></h1>
-	<h2 class = "baseText" style = "padding-top: 0px; margin-top:0px; color: #666; font-size:18px;">Welcome to Make It Happen</h2>
+	<h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:64px; text-shadow: 2px 2px 4px #000;"><span id = "LPNHS" onclick = "location.href='index.php'" title = "LPNHS - Home">Make It Happen</span></h1>
 	<?php if(isset($_SESSION["StudentID"])) : ?>
 		<div id = "headerLogout" class = "headerSignIn"><button id = "headerLogoutButton">Sign Out</button></div>
 	<?php else: ?>
@@ -13,16 +12,9 @@
     
 <div id = "navBarWrapper">
     <nav id = "navBar" class = "topnav">
-        <?php if(isset($_SESSION["StudentID"])) : ?>
             <a class = "baseText" id = "homeLink" href = "homePage.php">Home</a>
-            <a class = "baseText" id = "eventsLink" href = "events.php">Community Events</a>
-            <a class = "baseText" id = "membersLink" href = "members.php">Sponsorships</a>
-			<a class = "baseText" id = "myProfileLink" href = "my-profile.php">My Profile</a>
-        <?php else :?>
-            <a class = "baseText" id = "homeLink" href = "homePage.php">Home</a>
-            <a class = "baseText" id = "communityInvolvementLink" href = "community-involvement.php">Community Events</a>
-            <a class = "baseText" id = "membersLink" href = "members.php">Sponsorships</a>
-			<a class = "baseText" id = "whatItTakesLink" href = "what-it-takes.php">My Profile</a>
-        <?php endif; ?>
+            <a class = "baseText" id = "eventsLink" href = "events.php">Sponsor and View Events</a>
+            <a class = "baseText" id = "attendEventsLink" href = "attendEvents.php">Attend Events</a>
+
     </nav>
 </div>
