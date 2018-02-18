f8549ff84a46982edd87fced497d97ff98d8cc3d
Benwagrez <benwagrez@gmail.com>
2018-02-17 20:55:00 -0600

    footer finished

--- footer.php
+++ footer.php
@@ -3,10 +3,8 @@
     <!--Notice that this page is UNOFFICIAL-->
     <p style = "color: white;">***This website was created at <a href="https://codeday.org/chicago" style = "color: blue;">Codeday Chicago</a> 2018***</p>
 
-	<a style="text-decoration: none;
-    transition: 400ms;
-    color:white;" class="bottomtext" href="aboutUs.php">About Us</a>
+	<div width=100%; style="display: flex; text-align:center;"><a class="bottomtext" href="aboutUs.php">About Us</a>
 	<a class="bottomtext" href="privacyPolicy.php">Privacy Policy</a>
 	<a class="bottomtext" href="termsOfUse.php">Terms of Use</a>
-	<a class="bottomtext" href="contactUs.php">Contact Us</a>
+	<a class="bottomtext" href="contactUs.php">Contact Us</a></div>
     
