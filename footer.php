229a655f4b9f72955f4046817652c57c2976e772
Benwagrez <benwagrez@gmail.com>
2018-02-17 18:17:55 -0600

    Changed footer colors

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<h4 class = "baseText" style = "font-size: 24px">Make It Happen</h4>
+<h4 style="font-size: 24px;color: rgb(74, 202, 111);font-family: Bookman, sans-serif;text-align: center;">Make It Happen</h4>
     <p class = "footerText">Created by Ben, Kush, Luis, Salvador</p>
     
     <!--Notice that this page is UNOFFICIAL-->
