93b93a07021d73710cdd3896fe60afcaef97f0a5
Benwagrez <benwagrez@gmail.com>
2018-02-18 04:50:06 -0600

    Added green text to profile

--- userProfile.php
+++ userProfile.php
@@ -59,15 +59,15 @@ include 'database.php';?>
                 <img style="height:100%;width:100%;" src ="images/<?php echo $data[0][5]; ?>"/>
             </div>
             <div style = "Height: 100%;width:70%padding:10px;margin-top:15px;">
-			    <p style = "font-style: italic; font-size: 16px; text-align: left;">Welcome <?php echo $data[0][1]; ?>!</p>
-                <p style = "font-style: italic; font-size: 16px; text-align: left;">Name: <?php echo $data[0][1],' ',$data[0][2]; ?></p>
-                <p style = "font-style: italic; font-size: 16px; text-align: left;">Following: <?php for($x=0;$x<$following;$x++){echo $userFollowingArray[0][$x]; }?></p>
-                <p style = "font-style: italic; font-size: 16px; text-align: left;">Followers: <?php for($x=0;$x<$followed;$x++){echo $userFollowingArrays[0][$x]; }?></p>
-                <p style = "font-style: italic; font-size: 16px; text-align: left;">Current Projects/Events: <?php for($x=0;$x<$myEvents;$x++){echo $events[0][$x]; }?></p>
+			    <p style = "font-style: italic; font-size: 16px; text-align: left;color:rgb(100, 248, 144);text-shadow: 1px 1px 2px #000;">Welcome <?php echo '<a style="color:white;text-decoration:none;">',$data[0][1],'!</a>'; ?></p>
+                <p style = "font-style: italic; font-size: 16px; text-align: left;color:rgb(100, 248, 144);text-shadow: 1px 1px 2px #000;">Name: <?php echo '<a style="color:white;text-decoration:none;">',$data[0][1],' ',$data[0][2],'</a>'; ?></p>
+                <p style = "font-style: italic; font-size: 16px; text-align: left;color:rgb(100, 248, 144);text-shadow: 1px 1px 2px #000;">Following: <?php for($x=0;$x<$following;$x++){echo '<a style="color:white;text-decoration:none;">',$userFollowingArray[0][$x]," </a>"; }?></p>
+                <p style = "font-style: italic; font-size: 16px; text-align: left;color:rgb(100, 248, 144);text-shadow: 1px 1px 2px #000;">Followers: <?php for($x=0;$x<$followed;$x++){echo '<a style="color:white;text-decoration:none;">',$userFollowingArrays[0][$x]," </a>"; }?></p>
+                <p style = "font-style: italic; font-size: 16px; text-align: left;color:rgb(100, 248, 144);text-shadow: 1px 1px 2px #000;">Current Projects/Events: <?php for($x=0;$x<$myEvents;$x++){echo '<a style="color:white;text-decoration:none;">',$events[0][$x]," </a>"; }?></p>
                 <a type="submit" style = "color:rgb(100, 248, 144); text-shadow: 2px 2px 4px #000; value="Create +"></a>
             </div>
         </div>
-        <div id = "userComments" style = "width: 100%; height: 50%;">
+        <div id = "userComments" style = "width: 100%; height: 50%;padding:10px;">
             <p>
                 Comments go HERE!!!
             </p>
