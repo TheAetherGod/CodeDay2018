e1c3dbf15504ad8e9ea69da4672a01cbdaa935f8
Fuzzy31415 <fuzzyneto@gmail.com>
2018-02-18 01:53:20 -0600

    finished the profile corner(not that good tho)

--- header.php
+++ header.php
@@ -1,13 +1,21 @@
 <div id = "banner" style = "width:100%">
 	<img id = "LPLogo" src = "images/Logo.png" height="140" width="160" style="padding:0px;">
-        
+   <!-- isset($_SESSION["userID"]) -->
 <h1 class = "baseTitle" style = "padding-bottom: 0px; margin-bottom:0px; color: rgb(74, 202, 111);  font-size:64px; text-shadow: 2px 2px 4px #000;"><span id = "LPNHS" style = "cursor: pointer;" onclick = "location.href='homePage.php'">Make It Happen</span></h1>
-<?php if(isset($_SESSION["userID"])) : ?>
-	<div id = "headerLogout" class = "headerSignIn"><a class = "headerLogoutButton">User Profile</a></div>
+<?php if(true) : ?> 
+	<div id = "headerLogout" class = "headerSignIn" style = "width: 20%; height: 100px ;background-color: white;">
+         <div id = "profileBox" class = "card" style = "width: 100%; height: 100px; margin: 0px; text-shadow: 0px 0px 0px;box-shadow:0px 0px 0px; display: flex;">
+             <div id = "profilePic" style = "width: 40%;height: 100px" >
+                <img src ="images/placeHolderImage.jpg" style = "width = 100%; height: 50px;">
+             </div>
+            <a class = "headerLogoutButton" style = "width: 60%;color: rgb(74, 202, 111);background-color: white;">
+                 Nicolas C.
+            </a>
+        </div>
+    </div>
 	<?php else: ?>
 		<div id = "headerLogin" class = "headerSignIn"><a class = "headerLoginButton" href = "login.php" style="text-decoration:none;padding:5px;">Sign In</a></div>
 	<?php endif; ?>
-
 </div>
     
 <div id = "navBarWrapper">
