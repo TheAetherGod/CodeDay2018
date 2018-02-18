60d49890f3303c5ebcb3708fb7f8fe01eaf00bf1
Benwagrez <benwagrez@gmail.com>
2018-02-17 20:49:06 -0600

    Footer part 1

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
-<h4 style="font-size: 24px;color: rgb(74, 202, 111);font-family: Bookman, sans-serif;text-align: center;">Make It Happen</h4>
-    <p class = "footerText">Created by Ben, Kush, Luis, Salvador</p>
+<p class = "footerText">Created by Ben, Kush, Luis, Salvador</p>
     
     <!--Notice that this page is UNOFFICIAL-->
     <p style = "color: white;">***This website was created at <a href="https://codeday.org/chicago" style = "color: blue;">Codeday Chicago</a> 2018***</p>
+
+	<a style="text-decoration: none;
+    transition: 400ms;
+    color:white;" class="bottomtext" href="aboutUs.php">About Us</a>
+	<a class="bottomtext" href="privacyPolicy.php">Privacy Policy</a>
+	<a class="bottomtext" href="termsOfUse.php">Terms of Use</a>
+	<a class="bottomtext" href="contactUs.php">Contact Us</a>
+    
